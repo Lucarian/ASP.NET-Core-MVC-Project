649169efc0444c8ddeffdac0d88fa79246497ba4
gemGreece <scarlll@hotmail.com>
2019-06-02 13:02:05 +0300

    Scaffolding, CRUD, Model, Dependency Injection, Migration, SeedData file, etc.

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Red Cliff",
+                        ReleaseDate = DateTime.Parse("2008-3-2"),
+                        Genre = "Action",
+                        Price = 5.00M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Hero",
+                        ReleaseDate = DateTime.Parse("2002-2-26"),
+                        Genre = "Action",
+                        Price = 8.00M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Kung Fu Hustle",
+                        ReleaseDate = DateTime.Parse("2004-10-29"),
+                        Genre = "Comedy",
+                        Price = 7.00M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ip Man 3",
+                        ReleaseDate = DateTime.Parse("2015-3-29"),
+                        Genre = "Fighting",
+                        Price = 5.50M
+                    }
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
